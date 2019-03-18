e49a6dcabd7856e50904da407954d1ecc920cd40
Michael-Lemberger <44966756+michael-lemberger@users.noreply.github.com>
2019-03-18 22:08:31 +0200

    Update BasicCheck.sh

--- BasicCheck.sh
+++ BasicCheck.sh
@@ -5,6 +5,7 @@ DIR_PATH=${1?Error: no path given}
 PROGRAM=${2?Error: no program name}
 cd $DIR_PATH
 output="7"
+check="0"
 table=" Compilation     Memory leaks     thread race"
 
 make > "/dev/null"
@@ -16,19 +17,32 @@ fi
 
 valgrind --leak-check=full --error-exitcode=2 ./${PROGRAM} ${ARGUMANTS} > valgrind.txt  2>&1
 
-if [ $? -ne 2 ];then
-output="0"
-else
-output="2"
+if [ $? -eq 2 ];then
+check="1"
 fi
 
 valgrind --tool=helgrind --error-exitcode=1 ./${PROGRAM} ${ARGUMANTS} > helgrind.txt  2>&1
-if [ $? -eq 1 ];then
-output="1"
+if [ $? -ne 1 ];then
+if [ $check -ne 1];then
+echo $table
+echo "   PASS     PASS	      PASS"
+exit 0
+else
+echo $table
+echo "   PASS     FAIL	      PASS"
+exit 2
+fi
 fi
 
+if [ $check -eq 1];then
+output="3"
 echo $table
-echo "   PASS      FAIL	      FAIL"
+echo "   PASS     FAIL        FAIL"
+else
+output="1"
+echo $table
+echo "   PASS     PASS        FAIL"
+fi
 
 rm valgrind.txt
 rm helgrind.txt
